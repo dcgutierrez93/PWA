76e14696c35130cc91eea1a3c146de8ac67491d7
David Gutierrez <davidgutierrez@Davids-MacBook-Pro.local>
2019-01-06 13:34:57 -0600

    Updated Carosuel Layout

---
+++ public/scripts/custom.js
@@ -0,0 +1,8 @@
+// const delayLoading = 3000;
+//
+// setTimeout(function() {
+//     document.getElementById("loading-screen").style.display = "none";
+// }, delayLoading);
+$(document).ready(function() {
+    $('.carousel').carousel();
+});
